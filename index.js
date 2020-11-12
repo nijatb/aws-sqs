1960758f7554b7521f7f4e8aa853c9201322fb13
EC2 Default User <ec2-user@ip-172-31-16-56.ec2.internal>
2020-11-12 21:18:36 +0000

    sending 50 messages and processing them then deleting from SQS

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ AWS.config.region = 'us-east-1';
 
 var sqs = new AWS.SQS();
 
-var queueUrl;
 var params = {
   QueueName: 'test-queue',
   Attributes: {
@@ -12,9 +11,101 @@ var params = {
     VisibilityTimeout: '60'
   }
 };
+
+var queueUrl;
 sqs.createQueue(params, function(err, data) {
-  if (err) console.log(err, err.stack); 
+  if (err) console.log(err, err.stack);
   else {
-    console.log('Successfully created SQS queue URL '+ data.QueueUrl); 
+    console.log('Successfully created SQS queue URL ' + data.QueueUrl);
+    queueUrl = data.QueueUrl;
+    sendMessages(data.QueueUrl);
   }
 });
+
+// Send 50 messages to SQS
+async function sendMessages(queueUrl) {
+  var messages = [];
+  for (var i = 0; i < 5; i++) {
+    messages[i] = [];
+    for (var j = 0; j < 10; j++) {
+      messages[i][j] = 'Message number ' + (i * 10 + j) + '.';
+    }
+  }
+
+  // Asynchronously deliver messages in a batch of 10 x 5 times to SQS queue  
+  for (const message of messages) {
+    console.log('Sending message: ' + message)
+    params = {
+      Entries: [],
+      QueueUrl: queueUrl /* required */
+    };
+    for (var i = 0; i < 10; i++) {
+      params.Entries.push({
+        MessageBody: message[i],
+        Id: 'Message' + (messages.indexOf(message) * 10 + i)
+      });
+    }
+    await sqs.sendMessageBatch(params, function(err, data) {
+      if (err) console.log(err, err.stack);
+      else console.log(data);
+    });
+  }
+}
+
+
+var waitingSQS = false;
+var queueCounter = 0;
+
+setInterval(function() {
+  if (!waitingSQS) { // Still busy with previous request
+    if (queueCounter <= 0) {
+      receiveMessages();
+    }
+    else --queueCounter; // Reduce queue counter
+  }
+}, 1000);
+
+// Receive messages from queue
+function receiveMessages() {
+  var params = {
+    QueueUrl: queueUrl, // required
+    MaxNumberOfMessages: 10,
+    VisibilityTimeout: 60,
+    WaitTimeSeconds: 20 
+  };
+  waitingSQS = true;
+  sqs.receiveMessage(params, function(err, data) {
+    if (err) {
+      waitingSQS = false;
+      console.log(err, err.stack);
+    }
+    else {
+      waitingSQS = false;
+      if ((typeof data.Messages !== 'undefined') && (data.Messages.length !== 0)) {
+        console.log('Received '+data.Messages.length+' messages from SQS queue.');
+        processMessages(data.Messages);
+      }
+      else {
+        queueCounter = 60;
+        console.log('SQS queue empty, waiting for ' + queueCounter + 's.');
+      }
+    }
+  });
+}
+
+async function processMessages(messagesSQS) {
+  for (const item of messagesSQS) {
+    await console.log('Processing message: ' + item.Body); 
+    var params = {
+      QueueUrl: queueUrl,
+      ReceiptHandle: item.ReceiptHandle
+    }
+    await sqs.deleteMessage(params, function(err, data) {
+      if (err) console.log(err, err.stack);
+      else {
+        console.log('Deleted message RequestId: ' +
+          JSON.stringify(data.ResponseMetadata.RequestId));
+      }
+    })
+  }
+}
